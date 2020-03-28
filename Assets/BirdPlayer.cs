9b562cf0fef2a422be3b35e90f792b20c78facc2
Gatlin <gatlin.carrier@gmail.com>
2020-03-28 01:52:27 -0400

    added new movements for bird

--- Assets/BirdPlayer.cs
+++ Assets/BirdPlayer.cs
@@ -2,25 +2,45 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Player : MonoBehaviour
+public class BirdPlayer : MonoBehaviour
 {
     public float playerSpeed;
     public float jumpHeight;
+    public int numberOfFlaps = 0;
+    private Vector3 currentVelocity;
 
     private void Update()
     {
+        // Sprint: Can sprint in air. OnShiftDown increase speed, onShiftUp decrease speed
+        currentVelocity = this.gameObject.GetComponent<Rigidbody2D>().velocity;
+
         if (Input.GetKeyDown("d"))
         {
-            this.gameObject.GetComponent<Rigidbody2D>().velocity = new Vector2(playerSpeed,0);
+            this.gameObject.GetComponent<Rigidbody2D>().velocity = new Vector2(playerSpeed, currentVelocity.y);
         }
         if (Input.GetKeyDown("a"))
         {
-            this.gameObject.GetComponent<Rigidbody2D>().velocity = new Vector2(-playerSpeed, 0);
+            this.gameObject.GetComponent<Rigidbody2D>().velocity = new Vector2(-playerSpeed, currentVelocity.y);
         }
-        if (Input.GetKeyDown("w"))
+        if (Input.GetKeyDown("w") && numberOfFlaps < 5)
         {
+            numberOfFlaps++;
             this.gameObject.GetComponent<Rigidbody2D>().velocity = this.gameObject.GetComponent<Rigidbody2D>().velocity + new Vector2(0, jumpHeight);
         }
+      
+        
     }
+
+    
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.name == "floor")
+        {
+            numberOfFlaps = 0;
+        }
+    }
+
+    
 
 }
