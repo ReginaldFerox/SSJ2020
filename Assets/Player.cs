b84d073299cd24cd7aa50a753002217b97f92dcf
ReginaldFerox <59514515+ReginaldFerox@users.noreply.github.com>
2020-03-27 18:52:32 -0700

    BasicBirdMovement

---
+++ Assets/Player.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float playerSpeed;
+    public float jumpHeight;
+
+    private void Update()
+    {
+        if (Input.GetKeyDown("d"))
+        {
+            this.gameObject.GetComponent<Rigidbody2D>().velocity = new Vector2(playerSpeed,0);
+        }
+        if (Input.GetKeyDown("a"))
+        {
+            this.gameObject.GetComponent<Rigidbody2D>().velocity = new Vector2(-playerSpeed, 0);
+        }
+        if (Input.GetKeyDown("w"))
+        {
+            this.gameObject.GetComponent<Rigidbody2D>().velocity = this.gameObject.GetComponent<Rigidbody2D>().velocity + new Vector2(0, jumpHeight);
+        }
+    }
+
+}
