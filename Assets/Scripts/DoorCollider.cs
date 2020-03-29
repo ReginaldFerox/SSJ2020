c4b2bb6186102142a71a8719247959dd04547775
Gatlin <gatlin.carrier@gmail.com>
2020-03-29 11:56:24 -0400

    added door collider

---
+++ Assets/Scripts/DoorCollider.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorCollider : MonoBehaviour
+{
+
+    public GameObject HumanPlayer;
+    public GameObject Destination; // Gameobject where they will be teleported to
+    public bool doorIsOpen;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnCollisionEnter2D(Collision2D other)
+    {
+        Debug.Log(other);
+        if (other.gameObject.tag == "Player" && doorIsOpen) 
+        {
+            other.transform.position = Destination.transform.position;
+        }   
+       
+    }
+}
---
+++ Assets/Scripts/TeleportDestination.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TeleportDestination : MonoBehaviour
+{
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
